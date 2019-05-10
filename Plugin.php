db76ab6e33ca9c51e26f9c725d448f7351acbdb1
EdwardBock <edward.bock@palasthotel.de>
2019-05-10 14:51:36 +0200

    version update
    Signed-off-by: EdwardBock <edward.bock@palasthotel.de>

--- Plugin.php
+++ Plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Firebase Notifications
  * Plugin URI: https://github.com/palasthotel/grid-wordpress
  * Description: We will see...
- * Version: 0.4
+ * Version: 0.5
  * Author: Palasthotel <rezeption@palasthotel.de> (in person: Edward Bock)
  * Author URI: http://www.palasthotel.de
  * Requires at least: 5.0
