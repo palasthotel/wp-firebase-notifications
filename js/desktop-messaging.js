3409a5036fb5418bba4d446929d3b94a7daaff54
EdwardBock <edward.bock@palasthotel.de>
2019-11-21 09:20:06 +0100

    step by step

--- js/desktop-messaging.js
+++ js/desktop-messaging.js
@@ -23,11 +23,6 @@
 	const iconUrl = FirebaseMessagingWebapp.iconUrl;
 
 	// -----------------------------------
-	// initialize firebase
-	// -----------------------------------
-	firebase.initializeApp(firebaseConfig);
-
-	// -----------------------------------
 	// expose listener
 	// -----------------------------------
 	const buildListener = function(){
@@ -56,46 +51,74 @@
 	webapp.onMessage = onMessageListener.add;
 	webapp.offMessage= onMessageListener.remove;
 
-
 	// -----------------------------------
-	// initialize messaging
+	// initialize FCM
 	// -----------------------------------
-	const messaging = firebase.messaging();
+	const onInitListener = buildListener();
+	webapp.onFCMInitialized = onInitListener.add;
+	webapp.offFCMInitialized = onInitListener.remove;
 
-	let _token = null;
-	const getToken = () => _token;
+	function FCMApp(){
 
-	// request permission
-	messaging
-		.requestPermission()
-		.then(function(){
-			// permission granted
-			return messaging.getToken();
-		})
-		.then(function(token){
-			// save token on server?
-			_token = token;
-		})
-		.catch(function(){
-			// permission denied
-			console.error("Denied messaging");
+		console.log("initialize FCMApp");
+		// -----------------------------------
+		// initialize firebase
+		// -----------------------------------
+		firebase.initializeApp(firebaseConfig);
+
+		// -----------------------------------
+		// initialize messaging
+		// -----------------------------------
+		const messaging = firebase.messaging();
+
+		let _token = null;
+		function getToken(){ return _token; }
+
+		// request permission
+		messaging
+			.requestPermission()
+			.then(function(){
+				// permission granted
+				return messaging.getToken();
+			})
+			.then(function(token){
+				// save token on server?
+				_token = token;
+				onInitListener.each();
+			})
+			.catch(function(){
+				// permission denied
+				console.error("Denied messaging");
+			});
+
+		// wait for messages
+		messaging.onMessage(function(payload){
+
+			console.log("received message", payload.notification, payload.data);
+
+			onMessageListener.each(payload.notification, payload.data, payload.priority, payload);
+
+			const notification = new Notification(payload.notification.title, {
+				body: payload.notification.body,
+				icon: iconUrl,
+			});
+			notification.onclick = function(e){
+				window.open(payload.data.permalink,'_blank');
+			}
 		});
 
-	// wait for messages
-	messaging.onMessage(function(payload){
-
-		console.log("received message", payload.notification, payload.data);
-
-		onMessageListener.each(payload.notification, payload.data, payload.priority, payload);
-
-		const notification = new Notification(payload.notification.title, {
-			body: payload.notification.body,
-			icon: iconUrl,
-		});
-		notification.onclick = function(e){
-			window.open(payload.data.permalink,'_blank');
+		return {
+			getToken,
 		}
-	});
+	}
+
+	let _FCMAppInstance = null;
+	function firebaseInstance() {
+		if(!_FCMAppInstance){
+			_FCMAppInstance = FCMApp();
+		}
+		return _FCMAppInstance;
+	}
 
 	// -----------------------------------
 	// topic subscription api
@@ -103,6 +126,7 @@
 	const _isActiveValue = "1";
 	function setNotificationsEnabled(isEnabled){
 		if(isEnabled){
+			firebaseInstance();
 			localStorage.setItem("fcm-is-enabled", _isActiveValue)
 		}  else {
 			localStorage.removeItem("fcm-is-enabled");
@@ -111,15 +135,21 @@
 	function isNotificationsEnabled(){
 		return localStorage.getItem("fcm-is-enabled") === _isActiveValue;
 	}
-	function _getSubscriptionKey (token, topic){ return "fcm-"+token+"-is-subscribed-"+topic; }
-	function isSubscribed (token, topic){ return localStorage.getItem(_getSubscriptionKey(token, topic)) === "1";}
-	function setSubscribed (token, topic){ localStorage.setItem(_getSubscriptionKey(token, topic), _isActiveValue); }
-	function setUnsubscribed (token, topic){ localStorage.removeItem(_getSubscriptionKey(token, topic)); }
+	function _getSubscriptionKey (topic){
+		const token = firebaseInstance().getToken();
+		return "fcm-"+token+"-is-subscribed-"+topic;
+	}
+	function isSubscribed (topic){ return isNotificationsEnabled() && localStorage.getItem(_getSubscriptionKey(topic)) === "1";}
+	function setSubscribed (topic){ localStorage.setItem(_getSubscriptionKey(topic), _isActiveValue); }
+	function setUnsubscribed (topic){ localStorage.removeItem(_getSubscriptionKey(topic)); }
 
 
 	const cloudFunctionsBaseUrl = "https://us-central1-"+firebaseConfig.projectId+".cloudfunctions.net";
 
-	function request(action, token, topic){
+	async function request(action, topic){
+		console.log("request get token");
+		const token = firebaseInstance().getToken();
+		if(!token) throw "No request token";
 		return fetch(
 			cloudFunctionsBaseUrl+"/"+action+"?token="+token+"&topic="+topic,
 			{mode: 'no-cors',}
@@ -127,13 +157,13 @@
 	}
 
 	function subscribe (topic){
-		return request("subscribe", getToken(), topic).then(function(){
-			setSubscribed(getToken(), topic);
+		return request("subscribe", topic).then(function(){
+			setSubscribed(topic);
 		});
 	}
 	function unsubscribe(topic) {
-		return request("unsubscribe", getToken(), topic).then(function(){
-			setUnsubscribed(getToken(), topic);
+		return request("unsubscribe", topic).then(function(){
+			setUnsubscribed(topic);
 		});
 	}
 
@@ -143,8 +173,12 @@
 		isNotificationsEnabled,
 		subscribe,
 		unsubscribe,
-		isSubscribed: (topic) => isSubscribed(getToken(), topic),
+		isSubscribed,
 	};
+
+	if(isNotificationsEnabled()){
+		firebaseInstance();
+	}
 
 
 })( FirebaseMessagingWebapp, firebase);
